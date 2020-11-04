3a084b02993b5ed83d40d506d483f3c661fa2145
Marcus <marcusvinicius1_3@hotmail.com>
2020-11-04 08:56:52 -0300

    Iniciando um projeto apartir do projeto inicial (primeiro-projeto),. encontrado nesse mesmo repositório.

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { response, Router } from 'express';
+import { startOfHour, parseISO,  } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+
+const  appointmentsRouter = Router();
+const appointmentRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (require, response) => {
+  try {
+    const { provider, date } = require.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(appointmentRepository);
+
+    const appointment = createAppointment.execute({ provider, date:parsedDate });
+    return response.json(appointment);
+
+  } catch (error) {
+    return response.status(400).json({message: error.message});
+  }
+
+})
+
+export default appointmentsRouter;
